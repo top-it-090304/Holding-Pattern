c20ea919d58d973c2906b1eef94148eb7bfb5fcb
Ivan Ostroukhov <110439191+VaNNoSiC@users.noreply.github.com>
2026-02-12 19:20:23 +0600

    Add slowdown speed

--- scripts/Plane.gd
+++ scripts/Plane.gd
@@ -6,36 +6,38 @@ var speed: float = 0.4
 var forward: bool = true
 
 func setup_with_curve(curve: Curve2D):
- flight_path = curve
- position = curve.get_point_position(0)
+	flight_path = curve
+	position = curve.get_point_position(0)
 
 func _process(delta):
- if not flight_path: return
+	if not flight_path: return
 
 
 
- var slow_speed = speed * (0.2 + 0.5 * sin(t * PI))
+	var slow_speed = speed * (0.2 + 0.5 * sin(t * PI))
  
- if forward:
-  t += slow_speed * delta
-  if t >= 1.0:
-   t = 1.0
-   arrival()
- else:
-  t -= slow_speed * delta
-  if t <= 0.0:
-   t = 0.0
-   arrival()
+	if forward:
+		t += slow_speed * delta
+		if t >= 1.0:
+			t = 1.0
+			arrival()
+	else:
+		t -= slow_speed * delta
+		if t <= 0.0:
+			t = 0.0
+			arrival()
 
 
- var dist = t * flight_path.get_baked_length()
- var new_pos = flight_path.sample_baked(dist)
+	var dist = t * flight_path.get_baked_length()
+	var new_pos = flight_path.sample_baked(dist)
  
- look_at(new_pos)
- position = new_pos
+	look_at(new_pos)
+	position = new_pos
 
 func arrival():
- forward = !forward
- set_process(false)
- await get_tree().create_timer(0.8).timeout
- set_process(true)
+	forward = !forward
+	set_process(false)
+	await get_tree().create_timer(0.8).timeout
+	set_process(true)
+	
+	
