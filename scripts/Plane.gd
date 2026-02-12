3353fdebf8436906beba6c6c55b0fae7f9bb1566
Ivan Ostroukhov <110439191+VaNNoSiC@users.noreply.github.com>
2026-02-12 19:14:13 +0600

    Add files via upload

--- scripts/Plane.gd
+++ scripts/Plane.gd
@@ -1,27 +1,41 @@
 extends Sprite2D
 
-var flight_rout: Curve2D
-var t: float = 0.0
-var speed: float = 180.0
+var flight_path: Curve2D
+var t: float = 0.0         
+var speed: float = 0.4     
 var forward: bool = true
-var total_lenght: float = 0.0
-
 
 func setup_with_curve(curve: Curve2D):
-	flight_rout = curve
-	total_lenght = curve.get_baked_length()
-	position = curve.get_point_position(0)
- 
+ flight_path = curve
+ position = curve.get_point_position(0)
+
 func _process(delta):
-	if forward:
-		t += speed * delta
-		if t >= total_lenght:
-			forward = false
-	else:
-		t -= speed * delta
-		if t <= 0:
-			forward = true
+ if not flight_path: return
+
+
+
+ var slow_speed = speed * (0.2 + 0.5 * sin(t * PI))
  
-	var new_pos = flight_rout.sample_baked(t)
-	look_at(new_pos + (new_pos - position))
-	position = new_pos
+ if forward:
+  t += slow_speed * delta
+  if t >= 1.0:
+   t = 1.0
+   arrival()
+ else:
+  t -= slow_speed * delta
+  if t <= 0.0:
+   t = 0.0
+   arrival()
+
+
+ var dist = t * flight_path.get_baked_length()
+ var new_pos = flight_path.sample_baked(dist)
+ 
+ look_at(new_pos)
+ position = new_pos
+
+func arrival():
+ forward = !forward
+ set_process(false)
+ await get_tree().create_timer(0.8).timeout
+ set_process(true)
