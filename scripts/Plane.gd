902d5bd2ab056fb949f92d333baccb09304072f4
VaNNoSiC <ostr7557@gmail.com>
2026-03-08 00:20:35 +0600

    refactor: Change speed plane

--- scripts/Plane.gd
+++ scripts/Plane.gd
@@ -2,8 +2,8 @@ extends Sprite2D
 
 var current_route: Dictionary
 var t: float = 0.0         
-var speed: float = 180.0
-var current_speed: float = 0.0
+var target_speed: float = 90.0
+var current_speed: float = 0.0  
 var forward: bool = true
 var color: String
 
@@ -18,7 +18,7 @@ func setup_with_route(route_data: Dictionary, start_t: float = 0.0):
 	position = curve.sample_baked(dist)
 	
 	play_spawn_effect()
-	razgon_plane(3.5)
+	start_plane(3.5)
 
 func play_spawn_effect():
 	scale = Vector2.ZERO
@@ -30,10 +30,10 @@ func play_spawn_effect():
 	var flash = create_tween()
 	flash.tween_property(self, "modulate", final_color, 0.3)
 
-func razgon_plane(duration: float):
-	current_speed = 20.0
+
+func start_plane(duration: float):
 	var tween = create_tween().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
-	tween.tween_property(self, "current_speed_px", speed, duration)
+	tween.tween_property(self, "current_speed", target_speed, duration)
 
 func _process(delta):
 	if not current_route: return
@@ -42,16 +42,16 @@ func _process(delta):
 	var baked_length = curve.get_baked_length()
 	
 	var t_ = (current_speed * delta) / baked_length
-
-	var stop_plane = 0.15
+	
+	var stop_plane = 0.12
 	var distance_to_target = (1.0 - t) if forward else t
 	
 	if distance_to_target < stop_plane:
 		## снижение скорости
-		var slow_ = clamp(distance_to_target / stop_plane, 0.2, 1.0)
-		current_speed = lerp(current_speed, speed * slow_, 0.1)
-	else:
-		current_speed = lerp(current_speed, speed, 0.05)
+		var slow_factor = clamp(distance_to_target / stop_plane, 0.1, 1.0)
+		var target_brake_speed = target_speed * slow_factor
+		current_speed = lerp(current_speed, target_brake_speed, 0.1)
+	
 	
 	if forward:
 		t += t_
@@ -68,7 +68,6 @@ func _process(delta):
 	var new_pos = curve.sample_baked(dist)
 	
 	look_at(new_pos)
-	
 	position = new_pos
 
 func switch_to_next_route(arrived_at_end: bool):
@@ -81,7 +80,7 @@ func switch_to_next_route(arrived_at_end: bool):
 				next_route = route_data_item
 				break
 	
-
+	## остановка
 	set_process(false)
 	current_speed = 0.0
 	await get_tree().create_timer(0.8).timeout
@@ -97,5 +96,6 @@ func switch_to_next_route(arrived_at_end: bool):
 			forward = false
 		current_route = next_route
 	
-	razgon_plane(1.0) 
+	
+	start_plane(3.5)
 	set_process(true)
