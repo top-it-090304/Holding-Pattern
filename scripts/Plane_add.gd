2c57215861a6b1e5c56df1530689f5180f700ee0
VaNNoSiC <ostr7557@gmail.com>
2026-03-14 16:35:51 +0600

    fix: add plane

--- scripts/Plane_add.gd
+++ scripts/Plane_add.gd
@@ -2,7 +2,13 @@ extends TextureRect
 
 var is_dragging = false
 var ghost_plane: Sprite2D
-@onready var fly_count = $"../UI/TexturePlaneGhost"
+@onready var fly_count = %TexturePlaneGhost
+
+func _process(_delta):
+	if GameData.start_planes == 0:
+		texture = load("res://objects/count_fly_Zero.png")
+	if is_dragging and is_instance_valid(ghost_plane):
+		ghost_plane.global_position = get_viewport().get_mouse_position()
 
 
 func _gui_input(event):
@@ -17,18 +23,18 @@ func _create_ghost():
 	ghost_plane = Sprite2D.new()
 	ghost_plane.texture = fly_count.texture
 	ghost_plane.modulate = Color(1, 1, 1, 0.5)
+	get_parent().add_child(ghost_plane)
 	ghost_plane.top_level = true
-	get_tree().root.add_child(ghost_plane)
+	ghost_plane.z_index = 11
+	ghost_plane.scale = Vector2(1.0,1.0)
+	ghost_plane.global_position = get_viewport().get_mouse_position()
+	
 
-func _process(_delta):
-	if GameData.start_planes == 0:
-		texture = load("res://objects/count_fly_Zero.png")
-	if is_dragging and ghost_plane:
-		ghost_plane.global_position = get_global_mouse_position()
 
 func _drop_plane():
 	is_dragging = false
-	var mouse_pos = get_global_mouse_position()
+	var canvas_transform = get_viewport().get_canvas_transform()
+	var mouse_pos = canvas_transform.affine_inverse() * get_viewport().get_mouse_position()
 	var found_route = null
 	var route_data_ = null
 	var t_ = 0.0
@@ -45,9 +51,15 @@ func _drop_plane():
 				found_route = route
 				route_data_ = route.route_data
 				t_ = offset / curve.get_baked_length()
+				
+	if ghost_plane:
+		ghost_plane.queue_free()
+				
+
 
 	if found_route and route_data_:
 		found_route.spawn_plane(route_data_, t_)
 		print("самолет появился")
+		
 	
-	ghost_plane.queue_free()
+	
