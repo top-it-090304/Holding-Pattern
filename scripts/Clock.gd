a6a4df4d6800d08770550e449da96df8563d3166
VaNNoSiC <ostr7557@gmail.com>
2026-04-02 20:22:05 +0600

    UI: Hours of progress day

---
+++ scripts/Clock.gd
@@ -0,0 +1,32 @@
+extends Control
+
+@onready var arrow = $Arrow
+@onready var circle = $Circle
+@onready var riski = $Riski
+@onready var day_label = $UI/WeekDay
+
+var day_time: float = 10.0
+var current_time: float = 0.0
+
+var night_bg = Color(0.18, 0.157, 0.18, 1.0)
+var night_el = Color(0.647, 0.647, 0.647, 1.0)
+var day_bg = Color(0.822, 0.822, 0.822, 1.0)
+var day_el = Color(0.165, 0.149, 0.165, 1.0)
+
+func _process(delta: float):
+	current_time += delta
+	if current_time >= day_time:
+		current_time = 0.0
+		
+	var rotation_pct = current_time / day_time
+	arrow.rotation_degrees = rotation_pct * 360.0
+	
+	if rotation_pct > 0.5:
+		_transition_clock_theme(night_bg, night_el, delta)
+	else:
+		_transition_clock_theme(day_bg, day_el, delta)
+
+func _transition_clock_theme(target_bg: Color, target_elements: Color, delta: float):
+	circle.modulate = circle.modulate.lerp(target_bg, delta * 3.0)
+	riski.modulate = riski.modulate.lerp(target_elements, delta * 3.0)
+	arrow.modulate = arrow.modulate.lerp(target_elements, delta * 3.0)
