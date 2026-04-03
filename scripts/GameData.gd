a0e0f3ae9c006bb2f56625c6e1b2367976565d1c
VaNNoSiC <ostr7557@gmail.com>
2026-04-03 11:01:02 +0600

    UI: change Bonus screen

--- scripts/GameData.gd
+++ scripts/GameData.gd
@@ -1,8 +1,8 @@
 extends Node
 
 var start_planes: int = 3
-var big_planes: int = 0
-var max_passengers: int = 10
+var big_planes: int = 1
+var max_passengers: int = 6
 var current_week: int = 1
 
 var high_score: int = 0
